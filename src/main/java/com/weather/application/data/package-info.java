a25c022d77fa7444c5d98d143910a177be47764b
start.vaadin.com <no-reply@vaadin.com>
2024-07-02 10:16:14 +0000

    Generated project

---
+++ src/main/java/com/weather/application/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.weather.application.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/weather/application/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.weather.application.services;
+
+import org.springframework.lang.NonNullApi;
